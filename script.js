ec917b53908b526d3dc64fde1c9f0e45c4483394
Vtrbr2 <vrodriguespereira691@gmail.com>
2025-12-09 01:54:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,30 +4,3 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
 });
-// MOSTRA O LOADER POR 4s NA ENTRADA DO SITE
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        document.getElementById("global-loader").classList.add("hide");
-    }, 4000);
-});
-
-// ATIVA LOADER AO CLICAR EM LINKS OU BOTÕES
-document.addEventListener("click", function(e) {
-    const el = e.target.closest("a, button[data-loader]");
-
-    if (!el) return;
-
-    // Mostrar loader novamente
-    const loader = document.getElementById("global-loader");
-    loader.classList.remove("hide");
-
-    // Manter loader por 4 segundos antes de navegar
-    if (el.tagName.toLowerCase() === "a") {
-        e.preventDefault();
-        const href = el.getAttribute("href");
-
-        setTimeout(() => {
-            window.location.href = href;
-        }, 4000);
-    }
-});
